27a56f270ebbc280ea32df33a0b429c432c5e0a3
tbwit078 <tbwit078@learner.dvusd.org>
2022-03-22 09:26:09 -0700

    First Code

--- Json/Json/Program.cs
+++ Json/Json/Program.cs
@@ -3,14 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using static System.Console;
 
 namespace Json
 {
-    class Program
+    // <wf>
+    public class WeatherForecast
     {
-        static void Main(string[] args)
+        public DateTimeOffset Date { get; set; }
+        public int TemperatureCelsius { get; set; }
+        public string Summary { get; set; }
+    }
+    // </wf>
+
+    public class Program
+    {
+        public static void Main()
         {
+            var weatherForecast = new weatherForecast
+            {
+                Date = DateTime.Parse("2019-08-01"),
+                TemperatureCelsius = 25,
+                Summary = "Hot"
+            };
+            var weatherForecast1 = new weatherForecast
+            {
+                Date = DateTime.Parse("2019-08-02"),
+                TemperatureCelsius = 0,
+                Summary = "Cold"
+            };
+
+            string jsonString = JsonSerializer.Serialize(weatherForecast);
+            string jsonString1 = JsonSerializer.Serialize(weatherForecast1);
+
+            WriteLine(jsonString);
+            WriteLine(jsonString1);
+
+            weatherForecast w = JsonSerializer.Deserialize<weatherForecast>(jsonString1);
+            ReadKey();
         }
     }
 }
